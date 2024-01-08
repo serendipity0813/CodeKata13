62512da2e2fb482ce2c168ffb3bf054b4bb803c1
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2024-01-08 09:58:59 +0900

    2024.01.08

--- Level_80.cs
+++ Level_80.cs
@@ -11,8 +11,22 @@ namespace CodeKata13
         public static int[] Solution71(string today, string[] terms, string[] privacies)
         {
             int[] answer = new int[] { };
+            char key = 'A';
+            string year;
+            string month;
+            string day;
+            var customers = new Dictionary <string, string>();
 
-           
+            for(int i=0; i<privacies.Length; i++)
+            {
+                year = privacies[i].Substring(0, 4);
+                month = privacies[i].Substring(5, 2);
+                day = privacies[i].Substring(8, 2);
+                key = privacies[i][11];
+                Console.WriteLine(year + " " + month + " " + day +" " + key);
+                
+            }
+
             return answer;
         }
     }
